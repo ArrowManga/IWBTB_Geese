1991e94b8374cc645610218ad568f164e6fefbb1
ArrowManga <43293922+ArrowManga@users.noreply.github.com>
2018-09-15 21:30:44 +0900

    update1

---
+++ Assets/script/player.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour {
+
+    public float speed;
+    Vector3 position;
+
+    GameObject bullet;
+
+    Rigidbody2D rigid;
+
+    Animator animator;
+
+
+	// Use this for initialization
+	void Start () {
+        position = transform.position;
+        rigid = gameObject.GetComponent<Rigidbody2D>();
+        animator = gameObject.GetComponentInChildren<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        MoveUpdate();
+	}
+
+    void MoveUpdate()
+    {
+        animator.SetInteger("motion", 0);
+        if(Input.GetKey(KeyCode.LeftArrow))
+        {
+            animator.SetInteger("motion", 1);
+            transform.Translate(new Vector3(-1 * speed * Time.deltaTime, 0, 0));
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            animator.SetInteger("motion", 1);
+            transform.Translate(new Vector3(1 * speed * Time.deltaTime, 0, 0));
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+        if(Input.GetKey(KeyCode.Z))
+        {
+            animator.SetInteger("motion", 2);
+        }
+    }
+
+    void Jump()
+    {
+
+    }
+
+    void Shoot()
+    {
+
+    }
+}
